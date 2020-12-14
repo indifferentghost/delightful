e98c5746aeca16735cdaf775c4c8d93d6e5cc59b
Thomas Dillard <thomas.dillard.studios@gmail.com>
2020-12-13 22:49:01 -0700

    remove unused notifications and user

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -37,14 +37,14 @@ export default function Layout({ children, navigation }: LayoutProps) {
 							</div>
 							<div className="hidden lg:block lg:ml-4">
 								<div className="flex items-center">
-									<button className="bg-orange-500 flex-shrink-0 rounded-full p-1 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white">
+									{/* <button className="bg-orange-500 flex-shrink-0 rounded-full p-1 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white">
 										<span className="sr-only">View notifications</span>
 										<svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
 										</svg>
-									</button>
+									</button> */}
 
-									<div className="ml-3 relative flex-shrink-0">
+									{/* <div className="ml-3 relative flex-shrink-0">
 										<div>
 											<button className="bg-orange-500 rounded-full flex text-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white" id="user-menu" aria-haspopup="true">
 												<span className="sr-only">Open user menu</span>
@@ -64,7 +64,7 @@ export default function Layout({ children, navigation }: LayoutProps) {
 												Sign out
                   </a>
 										</div>
-									</div>
+									</div> */}
 								</div>
 							</div>
 						</div>
